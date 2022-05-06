a09f4c5a2085a2cbe411ed54928c618a8b6672c5
Bryan-CHM <chanhmbryan@gmail.com>
2022-05-07 04:26:31 +0800

    skeleton with pipelines

---
+++ src/main/java/VTTP/Project/VTTP_Project_One/VttpProjectOneApplication.java
@@ -0,0 +1,13 @@
+package VTTP.Project.VTTP_Project_One;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpProjectOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpProjectOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VTTP/Project/VTTP_Project_One/VttpProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package VTTP.Project.VTTP_Project_One;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
