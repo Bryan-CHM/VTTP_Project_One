9066f8494b90741cdef646bf0ec8d47becfd3154
Bryan-CHM <chanhmbryan@gmail.com>
2022-05-25 16:33:18 +0800

    The Most Final Commit

--- src/main/java/VTTP/Project/VTTP_Project_One/controllers/LoginController.java
+++ src/main/java/VTTP/Project/VTTP_Project_One/controllers/LoginController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import VTTP.Project.VTTP_Project_One.models.Animal;
@@ -41,6 +43,13 @@ public class LoginController {
     public ModelAndView createUserPage(){
         ModelAndView mvc = new ModelAndView();
         mvc.setViewName("create");
+        return mvc;
+    }
+
+    @GetMapping("/getuser")
+    public ModelAndView getUserPage(){
+        ModelAndView mvc = new ModelAndView();
+        mvc.setViewName("getuser");
         return mvc;
     }
     
@@ -100,11 +109,11 @@ public class LoginController {
         return mvc;
     }
 
-    @GetMapping("/user/{name}")
-    public ModelAndView viewUserFavourites(@PathVariable String name){
+    @GetMapping("/user")
+    public ModelAndView viewUserFavourites(@RequestParam String username){
         ModelAndView mvc = new ModelAndView();
         User user = new User();
-        user.setUsername(name);
+        user.setUsername(username);
         user.setPrivated(loginRepo.getUserPrivateSetting(user));
         mvc.addObject("user", user);
         Integer userId = loginRepo.checkIfUserExists(user);
